3782f0fd036db6bf53deafb4be8daf56743d8a86
Neil Mackey <neilmackey@gmail.com>
2021-12-13 20:30:08 +0000

    File scoped namespaces

--- authz/Startup.cs
+++ authz/Startup.cs
@@ -9,37 +9,36 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
 
-namespace authz
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+namespace authz;
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+public class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.Run(async context =>
         {
-            app.Run(async context =>
+            foreach (var header in context.Request.Headers)
             {
-                foreach (var header in context.Request.Headers)
-                {
-                    Console.WriteLine($"{header.Key}: {header.Value}");
-                }
+                Console.WriteLine($"{header.Key}: {header.Value}");
+            }
                 
-                if (context.Request.Query.TryGetValue("i_am_super_secret", out var secretValue) &&
-                    secretValue.Contains("yes"))
-                {
-                    await context.Response.CompleteAsync();
-                }
-                else
-                {
-                    context.Response.StatusCode = 403;
-                    await context.Response.WriteAsync("Forbidden");
-                }
-            });
-        }
+            if (context.Request.Query.TryGetValue("i_am_super_secret", out var secretValue) &&
+                secretValue.Contains("yes"))
+            {
+                await context.Response.CompleteAsync();
+            }
+            else
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Forbidden");
+            }
+        });
     }
 }
--- service/Startup.cs
+++ service/Startup.cs
@@ -9,35 +9,34 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace service
+namespace service;
+
+public class Startup
 {
-    public class Startup
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
+            app.UseDeveloperExceptionPage();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+        app.UseRouting();
+
+        app.Run(async context =>
+            await context.Response.WriteAsJsonAsync(new
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.Run(async context =>
-                    await context.Response.WriteAsJsonAsync(new
-                    {
-                        context.Request.Path,
-                        context.Request.Query,
-                        context.Request.Method,
-                        context.Request.Headers,
-                    })
-                );
-        }
+                context.Request.Path,
+                context.Request.Query,
+                context.Request.Method,
+                context.Request.Headers,
+            })
+        );
     }
 }
